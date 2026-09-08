c2ec8a27410e8194cf34c02165e59ea8d98af1c6
Salma-Srity <salmasrity10@gmail.com>
2026-09-08 16:40:41 +0600

    add variable practice program

---
+++ C_Basics/C_variables/calculate_using_variables.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(){
+    //find the area of a retangle
+    int length = 10;
+    int width  = 5;
+    int area ;
+
+    area = length * width;
+
+    printf("Area = %d",area);
+    return 0;
+
+}
